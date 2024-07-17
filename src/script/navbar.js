708f3e7589df29a75350ffdc5bd99d846a403f87
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-07-17 13:41:09 +0330

    fix user-interface

--- src/script/navbar.js
+++ src/script/navbar.js
@@ -2,3 +2,8 @@ function show() {
     const sidebar = document.querySelector(".sidebar__right");
     sidebar.style.display = 'flex';
 }
+function closesidebar() {
+    const sidebar = document.querySelector(".sidebar__right");
+    sidebar.style.display = 'none';
+    sidebar.style.transition = '3.s ease-out'
+}
