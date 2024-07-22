d41a8f87fe0aaae8d3a4de785a0a5ae5e2d9c492
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-07-22 10:31:41 +0330

    typewriter

--- src/script/navbar.js
+++ src/script/navbar.js
@@ -7,3 +7,33 @@ function closesidebar() {
     sidebar.style.display = 'none';
     sidebar.style.transition = '3.s ease-out'
 }
+// typewriter text 
+const words = ["خوش امدید", "به سایت ما خوش آمدید", "به وب سایت ما خوش آمدید"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 100);
+}
+
+type();
