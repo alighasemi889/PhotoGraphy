6f9b006ab74ea80131855444c6cb9b1161be2012
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-08-01 20:53:54 +0330

    Validateforms

--- src/script/validation.js
+++ src/script/validation.js
@@ -1,27 +1,30 @@
-"use strict";
-function validateform() {
-    let passwd = document.myform.password.value;
-    let email = document.myform.email.value;
-    let confirm = document.myform.confirm.value;
-    const span = document.getElementById("eror");
-    const pass = document.getElementById("pass");
-    const confirm__Txt = document.getElementById("confirm");
-    const eror__pass = document.getElementById("eror__pass");
-    if (passwd === "" || confirm === null || email === null) {
-     alert("هر دو فیلد اجباری است");
-    span.textContent = "ایمیل خالی است";
-    eror__pass.textContent = "رمز خالی است";
-    confirm__Txt.textContent = "خالی است ";
-    span.style.color = "red";
-    confirm__Txt.style.color = "red";
-    eror__pass.style.color = "red";
-    passwd.style.border = "2px solid red"
-    span.style.color = "red"
-    // eror__pass.textContent = "رمز خالی است"
-    return false;
-} else if(pass.length < 7){
-    alert("رمز خالی است")
+var form = document.getElementById("myform");
+form.addEventListener('submit', function aa(event) {
+    event.preventDefault();
+}) 
+
+let email = document.getElementById("email").value;
+let passwd = document.getElementById("password").value;
+const eror__pass = document.getElementById("eror__pass");
+
+    if (!emailIsValid(email)) {
+        alert("email is not valid")
+        // alert(email.value)
+
+        
+    }
+
+    if (passwd.length < 6) {
+        // eror__pass.textContent = "رمز خالی است";
+        // eror__pass.style.color = "red";
+        // passwd.style.borderBlockColor = "red";
+        alert("pass is not valid")
+        // alert("رمز خالی است");   
+       
+        form.submit();
+    }
+
+   
+function emailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
-}
-// اهنگ
-// یادت میاد تو میدون
