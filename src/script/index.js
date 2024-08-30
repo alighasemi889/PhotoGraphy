d3240793dfd1ea5230bcbcc5687dc2b64b7b7714
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-08-30 21:14:54 +0330

    styling sidebar and changing codes

--- src/script/index.js
+++ src/script/index.js
@@ -67,3 +67,14 @@ setTimeout(() => {
   }
   
 }, 4000);
+
+
+const sidebar = document.getElementById("menu");
+const hamberger = document.getElementById("hamberger__menu");
+const close_sidebar = document.getElementById("close_side_right");
+close_sidebar.onclick = () =>{
+  sidebar.style.display = "none"
+}
+hamberger.onclick = () =>{
+  sidebar.style.display = "block";
+}
