2848f820a0f7c472f7e5e8ccd619b6054ec4aed1
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-09-07 14:41:14 +0330

    add some  new sections

--- src/script/wedding.js
+++ src/script/wedding.js
@@ -1,15 +1,14 @@
 "use strict"
-// -----------------------------------------//
 //----------------- showing and hide the sidebar in mobile------------------
 const close__icon = document.getElementById("close_side_right");
-const menu = document.getElementById("menu");
+const menu__rightside = document.getElementById("menu");
 const hamberger__menu = document.getElementById("hamberger__menu");
 close__icon.onclick = () => {
-    menu.style.display = "none";
-    menu.style.transition = "3s ease-in all"
+    menu__rightside.style.display = "none";
+    menu__rightside.style.transition = "3s ease-in all"
 }
 hamberger__menu.onclick =  () =>{
-    menu.style.display = 'block';
-    menu.style.transitionBehavior = "4.s ease-in-out"
+    menu__rightside.style.display = 'block';
+    menu__rightside.style.transitionBehavior = "4.s ease-in-out"
 
 }
