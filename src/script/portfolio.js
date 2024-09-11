9899da8c93e5377ba424220549b3ac62006f59e6
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-09-11 12:48:06 +0330

    fix the bugs in all pages

---
+++ src/script/portfolio.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const sidebar3 = document.getElementById("menu");
+const hamberger = document.getElementById("hamberger__menu");
+const close_sidebar = document.getElementById("close_side_right");
+close_sidebar.onclick = () =>{
+  sidebar3.style.display = "none"
+}
+hamberger.onclick = () =>{
+  sidebar3.style.display = "block";
+}
+
