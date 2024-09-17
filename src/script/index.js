0707726723cf3979ad6eb31239bbfa08cb298965
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-09-17 13:07:18 +0330

    update codes and fix bugs in all pages

--- src/script/index.js
+++ src/script/index.js
@@ -1,46 +1,7 @@
 "use strict";
 
-// const { dotDecimal } = require("validator/lib/alpha");
 
-//  hide the sidebar
-
-
-// typewriter text 
-const words = ["خوش آمدید", "به سایت ما خوش آمدید", "به وب سایت ما خوش آمدید"];
-let i = 0;
-let j = 0;
-let currentWord = "";
-let isDeleting = false;
-
-function type() {
-  currentWord = words[i];
-  if (isDeleting) {
-    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
-    j--;
-    if (j == 0) {
-      isDeleting = false;
-      i++;
-      if (i == words.length) {
-        i = 0;
-      }
-    }
-  } else {
-    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
-    j++;
-    if (j == currentWord.length) {
-      isDeleting = true;
-    }
-  }
-  setTimeout(type, 100);
-}
-
-type();
-// --------------------dark mode toggle----------------
-
-// function darkmode() {
-//     const dark = document.body;
-//     dark.classList.toggle("dark-mode")
-// }
+// scrool top to bottom
 const scroolTop = () => {
   window.scrollTo({
     top : 0,
@@ -48,13 +9,6 @@ const scroolTop = () => {
   
   })
 }
-
-// const btn__form = document.getElementById("show_modal");
-// const form_modal = document.getElementById("main__form");
-// btn__form.onclick = function click() {
-//   form_modal.style.display = "block"
-// }
-
 // modal
 const modal = document.getElementById("modal");
 let close_modal = document.getElementById("close");
@@ -68,7 +22,7 @@ setTimeout(() => {
   
 }, 4000);
 
-
+// sidebar logic
 const sidebar = document.getElementById("menu");
 const hamberger = document.getElementById("hamberger__menu");
 const close_sidebar = document.getElementById("close_side_right");
@@ -79,4 +33,3 @@ hamberger.onclick = () =>{
   sidebar.style.display = "block";
 }
 
-// darkmode
