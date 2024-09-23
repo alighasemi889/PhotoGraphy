27a13d2ca212a559e4da4bf3bb3541d8cc96fec3
founder of codemaster <58810370+alighasemi889@users.noreply.github.com>
2024-09-23 14:23:03 +0330

    add progress bar in main page

--- src/script/portfolio.js
+++ src/script/portfolio.js
@@ -1,5 +1,5 @@
 "use strict"
-
+// showing sidebar and closing it
 const sidebar3 = document.getElementById("menu");
 const hamberger = document.getElementById("hamberger__menu");
 const close_sidebar = document.getElementById("close_side_right");
